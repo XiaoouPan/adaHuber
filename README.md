cc3be0461d64b9712cba781d1f7397f2208b0d84
Xiaoou Pan <xip024@ucsd.edu>
2019-07-23 13:32:45 -0700

    Code update

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ Tuning-Free Huber Estimation and Regression
 
 ## Description
 
-This package implements the Huber mean estimator, Huber covariance matrix estimation, adaptive Huber regression and l1-regularized Huber regression (Huber-Lasso) estimators efficiently. For all these methods, the robustification parameter tau is calibrated by a tuning-free principle.
+This package implements the Huber mean estimator, Huber covariance matrix estimation, adaptive Huber regression and l<sub>1</sub>-regularized Huber regression (Huber-Lasso) estimators efficiently. For all these methods, the robustification parameter tau is calibrated by a tuning-free principle.
 
-Specifically, for Huber regression, assume the observed data vectors (Y, X) follow a linear model Y = beta_0 + X * beta + epsilon, where Y is an n-dimensional response vector, X is an n by d design matrix, and epsilon is an n-vector of noise variables whose distributions can be asymmetric and/or heavy-tailed. The package computes the standard Huber's M-estimator when d < n and the Huber-Lasso estimator when d > n. The vector of coefficients beta and the intercept term beta_0 are estimated successively via a two-step procedure. See the reference paper Wang et al. (2018) for more details of the two-step tuning-free framework.
+Specifically, for Huber regression, assume the observed data vectors (Y, X) follow a linear model Y = &beta;<sub>0</sub> + X &beta; + &epsilon;, where Y is an n-dimensional response vector, X is an n by d design matrix, and &epsilon; is an n-vector of noise variables whose distributions can be asymmetric and/or heavy-tailed. The package computes the standard Huber's M-estimator when d < n and the Huber-Lasso estimator when d > n. The vector of coefficients beta and the intercept term &beta;<sub>0</sub> are estimated successively via a two-step procedure. See [Wang et al., 2018](https://www.math.ucsd.edu/~wez243/Tuning_Free.pdf) for more details of the two-step tuning-free framework.
 
 ## Installation
 
-Install `tfHuber` from github:
+Install `tfHuber` from GitHub:
 
-```{r gh-installation, eval = FALSE}
+```r
 install.packages("devtools")
 library(devtools)
 devtools::install_github("XiaoouPan/tfHuber")
@@ -29,14 +29,13 @@ For example, `?huberReg` will present a detailed documentation with inputs, outp
 
 The package `tfHuber` is implemented in `Rcpp` and `RcppArmadillo`, so the following error messages might appear when you first install it (we'll keep updating common error messages with feedback from users):
 
-* Error: "...could not find build tools necessary to build tfHuber": For Windows you need Rtools, for Mac OS X you need to install Command Line Tools for XCode. See (https://support.rstudio.com/hc/en-us/articles/200486498-Package-Development-Prerequisites). 
+* Error: "...could not find build tools necessary to build tfHuber": For Windows you need Rtools, for Mac OS X you need to install Command Line Tools for XCode. See [this link](https://support.rstudio.com/hc/en-us/articles/200486498-Package-Development-Prerequisites) for details. 
 
 * Error: "library not found for -lgfortran/-lquadmath": It means your gfortran binaries are out of date. This is a common environment specific issue. 
 
-    1. In R 3.0.0 - R 3.3.0: Upgrading to R 3.4 is strongly recommended. Then go to the next step. Alternatively, you can try the instructions here: http://thecoatlessprofessor.com/programming/rcpp-rcpparmadillo-and-os-x-mavericks-lgfortran-and-lquadmath-error/. 
+    1. In R 3.0.0 - R 3.3.0: Upgrading to R 3.4 is strongly recommended. Then go to the next step. Alternatively, you can try the instructions [here](http://thecoatlessprofessor.com/programming/rcpp-rcpparmadillo-and-os-x-mavericks-lgfortran-and-lquadmath-error/).
 
-    2. For >= R 3.4.* : download the installer from the here: https://gcc.gnu.org/wiki/GFortranBinaries#MacOS. Then run the installer.
-
+    2. For >= R 3.4.* : download the installer from [here](https://gcc.gnu.org/wiki/GFortranBinaries#MacOS). Then run the installer.
 
 ## Functions
 
@@ -49,9 +48,9 @@ There are four functions in this package:
 
 ## Examples 
 
-First, we present an example of Huber mean estimation. We generate data from a log-normal distribution, which is asymmetric and heavy-tailed. We estimate its mean by the tuning-free Huber mean estimator (Wang et al., 2018).
+First, we present an example of Huber mean estimation. We generate data from a log-normal distribution, which is asymmetric and heavy-tailed. We estimate its mean by the tuning-free Huber mean estimator.
 
-```{r}
+```r
 library(tfHuber)
 n = 1000
 X = rlnorm(n, 0, 1.5) - exp(1.5^2 / 2)
@@ -59,19 +58,19 @@ meanList = huberMean(X)
 hMean = meanList$mu
 ```
 
-Then we present an example of Huber covariance matrix estimation. We generate data from t distribution with df = 3, which is heavy-tailed. We estimate its covariance matrix by the method proposed in Ke et al., 2019.
+Then we present an example of Huber covariance matrix estimation. We generate data from t distribution with df = 3, which is heavy-tailed. We estimate its covariance matrix by the method proposed in [Ke et al., 2019](https://arxiv.org/abs/1811.01520).
 
-```{r}
+```r
 library(tfHuber)
-n = 400
+n = 100
 d = 50
 X = matrix(rt(n * d, df = 3), n, d) / sqrt(3)
 hubCov = huberCov(X)
 ```
 
-Next, we present an example of adaptive Huber regression. Here we generate data from a linear model Y = X * theta + epsilon, where epsilon follows a log-normal distribution, and estimate the intercept and coefficients by tuning-free Huber regression.
+Next, we present an example of adaptive Huber regression. Here we generate data from a linear model Y = X &theta; + &epsilon;, where &epsilon; follows a log-normal distribution, and estimate the intercept and coefficients by tuning-free Huber regression.
 
-```{r}
+```r
 library(tfHuber)
 n = 500
 d = 5
@@ -83,12 +82,12 @@ listHuber = huberReg(X, Y)
 thetaHuber = listHuber$theta
 ```
 
-Finally, we illustrate the use of l1-regularized Huber regression. Again, we generate data from a linear model Y = X * theta + epsilon, where theta is a high-dimensional vector, and epsilon is from a log-normal distribution. We estimate the intercept and coefficients by Huber-Lasso regression, where the regularization parameter lambda is calibrated by K-fold cross-validation, and the robustification parameter tau is chosen by a tuning-free procedure.
+Finally, we illustrate the use of l<sub>1</sub>-regularized Huber regression. Again, we generate data from a linear model Y = X &theta; + &epsilon;, where &theta; is a high-dimensional vector, and &epsilon; is from a log-normal distribution. We estimate the intercept and coefficients by Huber-Lasso regression, where the regularization parameter &lambda; is calibrated by K-fold cross-validation, and the robustification parameter &tau; is chosen by a tuning-free procedure.
 
-```{r}
+```r
 library(tfHuber)
-n = 200
-d = 500
+n = 100
+d = 200
 s = 5
 thetaStar = c(rep(3, s + 1), rep(0, d - s))
 X = matrix(rnorm(n * d), n, d)
@@ -108,20 +107,20 @@ Xiaoou Pan <xip024@ucsd.edu>, Wen-Xin Zhou <wez243@ucsd.edu>
 
 ## References
 
-Eddelbuettel, D. and Francois, R. (2011). Rcpp: Seamless R and C++ Integration. J. Stat. Softw. 40(8) 1-18. (http://dirk.eddelbuettel.com/code/rcpp/Rcpp-introduction.pdf)
+Eddelbuettel, D. and Francois, R. (2011). Rcpp: Seamless R and C++ Integration. J. Stat. Softw. 40(8) 1-18. [Paper](http://dirk.eddelbuettel.com/code/rcpp/Rcpp-introduction.pdf)
 
-Eddelbuettel, D. and Sanderson, C. (2014). RcppArmadillo: Accelerating R with high-performance C++ linear algebra. Comp. Stat. Dat. Ana. 71 1054-1063. (http://dirk.eddelbuettel.com/papers/RcppArmadillo.pdf)
+Eddelbuettel, D. and Sanderson, C. (2014). RcppArmadillo: Accelerating R with high-performance C++ linear algebra. Comp. Stat. Dat. Ana. 71 1054-1063. [Paper](http://dirk.eddelbuettel.com/papers/RcppArmadillo.pdf)
 
-Fan, J., Liu, H., Sun, Q. and Zhang, T. (2018). I-LAMM for sparse learning: Simultaneous control of algorithmic complexity and statistical error. Ann. Statist. 46 814–841. (https://projecteuclid.org/euclid.aos/1522742437)
+Fan, J., Liu, H., Sun, Q. and Zhang, T. (2018). I-LAMM for sparse learning: Simultaneous control of algorithmic complexity and statistical error. Ann. Statist. 46 814–841. [Paper](https://projecteuclid.org/euclid.aos/1522742437)
 
-Ke, Y., Minsker, S., Ren, Z., Sun, Q. and Zhou, W.-X. (2019). User-Friendly Covariance Estimation for Heavy-Tailed Distributions: A Survey and Recent Results. Statis. Sci. To appear. (https://arxiv.org/abs/1811.01520)
+Ke, Y., Minsker, S., Ren, Z., Sun, Q. and Zhou, W.-X. (2019). User-Friendly Covariance Estimation for Heavy-Tailed Distributions: A Survey and Recent Results. Statis. Sci. To appear. [Paper](https://arxiv.org/abs/1811.01520)
 
-Pan, X., Sun, Q. and Zhou, W.-X. (2019). Nonconvex regularized robust regression with oracle properties in polynomial time. Preprint. (https://arxiv.org/abs/1907.04027).
+Pan, X., Sun, Q. and Zhou, W.-X. (2019). Nonconvex regularized robust regression with oracle properties in polynomial time. Preprint. [Paper](https://arxiv.org/abs/1907.04027).
 
-Sanderson, C. and Curtin, R. (2016). Armadillo: a template-based C++ library for linear algebra. J. Open. Src. Softw. 1(2) 26. (http://conradsanderson.id.au/pdfs/sanderson_armadillo_joss_2016.pdf)
+Sanderson, C. and Curtin, R. (2016). Armadillo: a template-based C++ library for linear algebra. J. Open. Src. Softw. 1(2) 26. [Paper](http://conradsanderson.id.au/pdfs/sanderson_armadillo_joss_2016.pdf)
 
-Sun, Q., Zhou, W.-X. and Fan, J. (2018). Adaptive Huber regression. J. Amer. Stat. Assoc, to appear. (https://www.tandfonline.com/doi/abs/10.1080/01621459.2018.1543124)
+Sun, Q., Zhou, W.-X. and Fan, J. (2018). Adaptive Huber regression. J. Amer. Stat. Assoc. 0 1-12. [Paper](https://www.tandfonline.com/doi/abs/10.1080/01621459.2018.1543124)
 
-Tibshirani, R. (1996). Regression shrinkage and selection via the lasso. J. R. Stat. Soc. Ser. B. Stat. Methodol. 58 267–288. (https://www.jstor.org/stable/2346178?seq=1#metadata_info_tab_contents)
+Tibshirani, R. (1996). Regression shrinkage and selection via the lasso. J. R. Stat. Soc. Ser. B. Stat. Methodol. 58 267–288. [Paper](https://www.jstor.org/stable/2346178?seq=1#metadata_info_tab_contents)
 
-Wang, L., Zheng, C., Zhou, W. and Zhou, W.-X. (2018). A new principle for tuning-free Huber regression. Preprint. (https://www.math.ucsd.edu/~wez243/Tuning_Free.pdf)
+Wang, L., Zheng, C., Zhou, W. and Zhou, W.-X. (2018). A new principle for tuning-free Huber regression. Preprint. [Paper](https://www.math.ucsd.edu/~wez243/Tuning_Free.pdf)
--- src/tfHuber.cpp
+++ src/tfHuber.cpp
@@ -3,18 +3,17 @@
 # include <string>
 // [[Rcpp::depends(RcppArmadillo)]]
 
-// [[Rcpp::export]]
 double f1(const double x, const arma::vec& resSq, const int n) {
   return arma::sum(arma::min(resSq, x * arma::ones(n))) / (n * x) - std::log(n) / n;
 }
 
-// [[Rcpp::export]]
 double rootf1(const arma::vec& resSq, const int n, double low, double up, 
               const double tol = 0.00001, const int maxIte = 500) {
   int ite = 0;
+  double mid, val;
   while (ite <= maxIte && up - low > tol) {
-    double mid = (up + low) / 2;
-    double val = f1(mid, resSq, n);
+    mid = (up + low) / 2;
+    val = f1(mid, resSq, n);
     if (val == 0) {
       return mid;
     } else if (val < 0) {
@@ -27,20 +26,19 @@ double rootf1(const arma::vec& resSq, const int n, double low, double up,
   return (low + up) / 2;
 }
 
-// [[Rcpp::export]]
 double f2(const double x, const arma::vec& resSq, const int n, const int d) {
   int N = n * (n - 1) >> 1;
   return arma::sum(arma::min(resSq, x * arma::ones(N))) / (N * x) - 
     (2 * std::log(d) + std::log(n)) / n;
 }
 
-// [[Rcpp::export]]
 double rootf2(const arma::vec& resSq, const int n, const int d, double low, double up, 
               const double tol = 0.00001, const int maxIte = 500) {
   int ite = 0;
+  double mid, val;
   while (ite <= maxIte && up - low > tol) {
-    double mid = (up + low) / 2;
-    double val = f2(mid, resSq, n, d);
+    mid = (up + low) / 2;
+    val = f2(mid, resSq, n, d);
     if (val == 0) {
       return mid;
     } else if (val < 0) {
@@ -83,13 +81,14 @@ Rcpp::List huberMean(const arma::vec& X, const double epsilon = 0.00001, const i
   double tauOld = 0;
   double tauNew = arma::stddev(X) * std::sqrt((long double)n / std::log(n));
   int iteNum = 0;
+  arma::vec res(n), resSq(n), w(n);
   while (((std::abs(muNew - muOld) > epsilon) || (std::abs(tauNew - tauOld) > epsilon)) && iteNum < iteMax) {
     muOld = muNew;
     tauOld = tauNew;
-    arma::vec res = X - muOld * arma::ones(n);
-    arma::vec resSq = arma::square(res);
+    res = X - muOld * arma::ones(n);
+    resSq = arma::square(res);
     tauNew = std::sqrt((long double)rootf1(resSq, n, arma::min(resSq), arma::sum(resSq)));
-    arma::vec w = arma::min(tauNew * arma::ones(n) / arma::abs(res), arma::ones(n));
+    w = arma::min(tauNew * arma::ones(n) / arma::abs(res), arma::ones(n));
     muNew = arma::as_scalar(X.t() * w) / arma::sum(w);
     iteNum++;
   }
@@ -97,7 +96,6 @@ Rcpp::List huberMean(const arma::vec& X, const double epsilon = 0.00001, const i
                             Rcpp::Named("iteration") = iteNum);
 }
 
-// [[Rcpp::export]]
 double hMeanCov(const arma::vec& Z, const int n, const int d, const double epsilon = 0.00001, 
                     const int iteMax = 500) {
   int N = Z.size();
@@ -106,13 +104,14 @@ double hMeanCov(const arma::vec& Z, const int n, const int d, const double epsil
   double tauOld = 0;
   double tauNew = arma::stddev(Z) * std::sqrt((long double)n / (2 * std::log(d) + std::log(n)));
   int iteNum = 0;
+  arma::vec res(n), resSq(n), w(n);
   while (((std::abs(muNew - muOld) > epsilon) || (std::abs(tauNew - tauOld) > epsilon)) && iteNum < iteMax) {
     muOld = muNew;
     tauOld = tauNew;
-    arma::vec res = Z - muOld * arma::ones(N);
-    arma::vec resSq = arma::square(res);
+    res = Z - muOld * arma::ones(N);
+    resSq = arma::square(res);
     tauNew = std::sqrt((long double)rootf2(resSq, n, d, arma::min(resSq), arma::sum(resSq)));
-    arma::vec w = arma::min(tauNew * arma::ones(N) / arma::abs(res), arma::ones(N));
+    w = arma::min(tauNew * arma::ones(N) / arma::abs(res), arma::ones(N));
     muNew = arma::as_scalar(Z.t() * w) / arma::sum(w);
     iteNum++;
   }
@@ -132,7 +131,7 @@ double hMeanCov(const arma::vec& Z, const int n, const int d, const double epsil
 //' @references Wang, L., Zheng, C., Zhou, W. and Zhou, W.-X. (2018). A New Principle for Tuning-Free Huber Regression. Preprint.
 //' @references Ke, Y., Minsker, S., Ren, Z., Sun, Q. and Zhou, W.-X. (2019). User-Friendly Covariance Estimation for Heavy-Tailed Distributions: A Survey and Recent Results. Statis. Sci. To appear. 
 //' @examples
-//' n = 400
+//' n = 100
 //' d = 50
 //' X = matrix(rt(n * d, df = 3), n, d) / sqrt(3)
 //' hubCov = huberCov(X)
@@ -190,27 +189,30 @@ arma::mat huberCov(const arma::mat& X, const double epsilon = 0.00001, const int
 //' @export
 // [[Rcpp::export]]
 Rcpp::List huberReg(const arma::mat& X, const arma::vec& Y, const double epsilon = 0.00001, 
-                        const double constTau = 1.345, const int iteMax = 500) {
+                    const double constTau = 1.345, const int iteMax = 500) {
   int n = X.n_rows;
   int d = X.n_cols;
-  arma::mat Z = arma::ones(n, d + 1);
+  arma::mat Z(n, d + 1);
   Z.cols(1, d) = X;
+  Z.col(0) = arma::ones(n);
   arma::vec thetaOld = arma::zeros(d + 1);
   arma::vec thetaNew = arma::solve(Z.t() * Z, Z.t() * Y);
   double tauOld = 0;
   double tauNew = std::sqrt((long double)arma::sum(arma::square(Y - Z * thetaNew)) / (n - d)) *
     std::sqrt((long double)n / std::log((long double)(d + std::log(n * d))));
-  double mad = 0;
+  double mad;
   int iteNum = 0;
+  arma::vec res(n), WY(n);
+  arma::mat WZ(n, d + 1);
   while ((arma::norm(thetaNew - thetaOld, "inf") > epsilon || std::abs(tauNew - tauOld) > epsilon) 
            && iteNum < iteMax) {
     thetaOld = thetaNew;
     tauOld = tauNew;
-    arma::vec res = Y - Z * thetaOld;
+    res = Y - Z * thetaOld;
     mad = arma::median(arma::abs(res - arma::median(res))) / 0.6744898;
     tauNew = constTau * mad;
-    arma::mat WZ = Z;
-    arma::vec WY = Y;
+    WZ = Z;
+    WY = Y;
     for (int i = 0; i < n; i++) {
       double w = tauNew / std::abs(res(i));
       if (w < 1) {
@@ -228,24 +230,20 @@ Rcpp::List huberReg(const arma::mat& X, const arma::vec& Y, const double epsilon
                             Rcpp::Named("iteItcp") = listMean["iteration"]);
 }
 
-// [[Rcpp::export]]
 int sgn(const double x) {
   return (x > 0) - (x < 0);
 }
 
-// [[Rcpp::export]]
 arma::vec softThresh(const arma::vec& x, const arma::vec& lambda) {
   return arma::sign(x) % arma::max(arma::abs(x) - lambda, arma::zeros(x.size()));
 }
 
-// [[Rcpp::export]]
 arma::vec cmptLambda(const arma::vec& beta, const double lambda) {
   arma::vec rst = lambda * arma::ones(beta.size());
   rst(0) = 0;
   return rst;
 }
 
-// [[Rcpp::export]]
 double loss(const arma::vec& Y, const arma::vec& Ynew, const std::string lossType,
             const double tau) {
   double rst = 0;
@@ -265,7 +263,6 @@ double loss(const arma::vec& Y, const arma::vec& Ynew, const std::string lossTyp
   return rst;
 }
 
-// [[Rcpp::export]]
 arma::vec gradLoss(const arma::mat& X, const arma::vec& Y, const arma::vec& beta,
                    const std::string lossType, const double tau) {
   arma::vec res = Y - X * beta;
@@ -284,7 +281,6 @@ arma::vec gradLoss(const arma::mat& X, const arma::vec& Y, const arma::vec& beta
   return rst / Y.size();
 }
 
-// [[Rcpp::export]]
 arma::vec updateBeta(const arma::mat& X, const arma::vec& Y, arma::vec beta, const double phi,
                      const arma::vec& Lambda, const std::string lossType, const double tau) {
   arma::vec first = beta - gradLoss(X, Y, beta, lossType, tau) / phi;
@@ -292,7 +288,6 @@ arma::vec updateBeta(const arma::mat& X, const arma::vec& Y, arma::vec beta, con
   return softThresh(first, second);
 }
 
-// [[Rcpp::export]]
 double cmptPsi(const arma::mat& X, const arma::vec& Y, const arma::vec& betaNew,
                const arma::vec& beta, const double phi, const std::string lossType,
                const double tau) {
@@ -303,16 +298,16 @@ double cmptPsi(const arma::mat& X, const arma::vec& Y, const arma::vec& betaNew,
     return rst;
 }
 
-// [[Rcpp::export]]
 Rcpp::List LAMM(const arma::mat& X, const arma::vec& Y, const arma::vec& Lambda, arma::vec beta,
                 const double phi, const std::string lossType, const double tau, 
                 const double gamma) {
   double phiNew = phi;
   arma::vec betaNew = arma::vec();
+  double FVal, PsiVal;
   while (true) {
     betaNew = updateBeta(X, Y, beta, phiNew, Lambda, lossType, tau);
-    double FVal = loss(Y, X * betaNew, lossType, tau);
-    double PsiVal = cmptPsi(X, Y, betaNew, beta, phiNew, lossType, tau);
+    FVal = loss(Y, X * betaNew, lossType, tau);
+    PsiVal = cmptPsi(X, Y, betaNew, beta, phiNew, lossType, tau);
     if (FVal <= PsiVal) {
       break;
     }
@@ -321,7 +316,6 @@ Rcpp::List LAMM(const arma::mat& X, const arma::vec& Y, const arma::vec& Lambda,
   return Rcpp::List::create(Rcpp::Named("beta") = betaNew, Rcpp::Named("phi") = phiNew);
 }
 
-// [[Rcpp::export]]
 arma::vec lasso(const arma::mat& X, const arma::vec& Y, const double lambda,
                 const double phi0 = 0.001, const double gamma = 1.5, 
                 const double epsilon_c = 0.001, const int iteMax = 500) {
@@ -331,9 +325,10 @@ arma::vec lasso(const arma::mat& X, const arma::vec& Y, const double lambda,
   arma::vec Lambda = cmptLambda(beta, lambda);
   double phi = phi0;
   int ite = 0;
+  Rcpp::List listLAMM;
   while (ite < iteMax) {
     ite++;
-    Rcpp::List listLAMM = LAMM(X, Y, Lambda, beta, phi, "l2", 1, gamma);
+    listLAMM = LAMM(X, Y, Lambda, beta, phi, "l2", 1, gamma);
     betaNew = Rcpp::as<arma::vec>(listLAMM["beta"]);
     phi = listLAMM["phi"];
     phi = std::max(phi0, phi / gamma);
@@ -345,26 +340,29 @@ arma::vec lasso(const arma::mat& X, const arma::vec& Y, const double lambda,
   return betaNew;
 }
 
-// [[Rcpp::export]]
 Rcpp::List huberLasso(const arma::mat& X, const arma::vec& Y, const double lambda,
                       double tau = -1, const double constTau = 1.345, const double phi0 = 0.001, 
                       const double gamma = 1.5, const double epsilon_c = 0.001, 
                       const int iteMax = 500) {
+  int n = X.n_rows;
   int d = X.n_cols - 1;
   arma::vec beta = arma::zeros(d + 1);
   arma::vec betaNew = arma::zeros(d + 1);
   arma::vec Lambda = cmptLambda(beta, lambda);
-  if (tau < 0) {
+  double mad;
+  if (tau <= 0) {
     arma::vec betaLasso = lasso(X, Y, lambda, phi0, gamma, epsilon_c, iteMax);
     arma::vec res = Y - X * betaLasso;
-    double mad = arma::median(arma::abs(res - arma::median(res))) / 0.6744898;
+    mad = arma::median(arma::abs(res - arma::median(res))) / 0.6744898;
     tau = constTau * mad;
   }
   double phi = phi0;
   int ite = 0;
+  Rcpp::List listLAMM;
+  arma::vec res(n);
   while (ite < iteMax) {
     ite++;
-    Rcpp::List listLAMM = LAMM(X, Y, Lambda, beta, phi, "Huber", tau, gamma);
+    listLAMM = LAMM(X, Y, Lambda, beta, phi, "Huber", tau, gamma);
     betaNew = Rcpp::as<arma::vec>(listLAMM["beta"]);
     phi = listLAMM["phi"];
     phi = std::max(phi0, phi / gamma);
@@ -372,27 +370,24 @@ Rcpp::List huberLasso(const arma::mat& X, const arma::vec& Y, const double lambd
       break;
     }
     beta = betaNew;
-    arma::vec res = Y - X * beta;
-    double mad = arma::median(arma::abs(res - arma::median(res))) / 0.6744898;
+    res = Y - X * beta;
+    mad = arma::median(arma::abs(res - arma::median(res))) / 0.6744898;
     tau = constTau * mad;
   }
   return Rcpp::List::create(Rcpp::Named("beta") = betaNew, Rcpp::Named("tau") = tau,
                             Rcpp::Named("iteration") = ite);
 }
 
-// [[Rcpp::export]]
 arma::uvec getIndex(const int n, const int low, const int up) {
   arma::vec seq = arma::regspace(0, n - 1);
   return arma::find(seq >= low && seq <= up);
 }
 
-// [[Rcpp::export]]
 arma::uvec getIndexComp(const int n, const int low, const int up) {
   arma::vec seq = arma::regspace(0, n - 1);
   return arma::find(seq < low || seq > up);
 }
 
-// [[Rcpp::export]]
 double pairPred(const arma::mat& X, const arma::vec& Y, const arma::vec& beta) {
   int n = X.n_rows;
   int d = X.n_cols - 1;
@@ -415,7 +410,7 @@ double pairPred(const arma::mat& X, const arma::vec& Y, const arma::vec& beta) {
 //' The observed data are \eqn{(Y, X)}, where \eqn{Y} is an \eqn{n}-dimensional response vector and \eqn{X} is an \eqn{n} by \eqn{d} design matrix. We assume that \eqn{Y} depends on \eqn{X} through a linear model \eqn{Y = X \beta + \epsilon}, where \eqn{\beta} is a sparse vector and \eqn{\epsilon} is an \eqn{n}-dimensional noise vector whose distribution can be asymmetrix and/or heavy-tailed. All the arguments except for \eqn{X} and \eqn{Y} have default settings.
 //'
 //' @title Tuning-free Huber-Lasso regression
-//' @param X An \eqn{n} by \eqn{d} design matrix with each row being a sample and each column being a variable, either low-dimensional data (\eqn{d \le n}) or high-dimensional data (\eqn{d > n}) are allowed..
+//' @param X An \eqn{n} by \eqn{d} design matrix with each row being a sample and each column being a variable, either low-dimensional data (\eqn{d \le n}) or high-dimensional data (\eqn{d > n}) are allowed.
 //' @param Y A continuous response vector with length \eqn{n}.
 //' @param lSeq Sequence of tuning parameter of regularized regression \eqn{\lambda}, every element should be positive. If it's not specified, the default sequence is generated in this way: define \eqn{\lambda_max = max(|Y^T X|) / n}, and \eqn{\lambda_min = 0.01 * \lambda_max}, then \code{lseq} is a sequence from \eqn{\lambda_max} to \eqn{\lambda_min} that decreases uniformly on log scale.
 //' @param nlambda Number of \eqn{\lambda} to generate the default sequence \code{lSeq}. It's not necessary if \code{lSeq} is specified. The default value is 30.
@@ -440,8 +435,8 @@ double pairPred(const arma::mat& X, const arma::vec& Y, const arma::vec& beta) {
 //' @references Fan, J., Liu, H., Sun, Q. and Zhang, T. (2018). I-LAMM for sparse learning: Simultaneous control of algorithmic complexity and statistical error. Ann. Statist. 46 814–841.
 //' @seealso \code{\link{huberReg}}
 //' @examples
-//' n = 200
-//' d = 500
+//' n = 100
+//' d = 200
 //' s = 5
 //' thetaStar = c(rep(3, s + 1), rep(0, d - s))
 //' X = matrix(rnorm(n * d), n, d)
@@ -453,13 +448,14 @@ double pairPred(const arma::mat& X, const arma::vec& Y, const arma::vec& beta) {
 // [[Rcpp::export]]
 Rcpp::List cvHuberLasso(const arma::mat& X, const arma::vec& Y,
                         Rcpp::Nullable<Rcpp::NumericVector> lSeq = R_NilValue, int nlambda = 30,
-                        const double constTau = 1.345, const double phi0 = 0.001, 
+                        const double constTau = 2.5, const double phi0 = 0.001, 
                         const double gamma = 1.5, const double epsilon_c = 0.001, 
                         const int iteMax = 500, int nfolds = 3) {
   int n = X.n_rows;
   int d = X.n_cols;
-  arma::mat Z = arma::ones(n, d + 1);
+  arma::mat Z(n, d + 1);
   Z.cols(1, d) = X;
+  Z.col(0) = arma::ones(n);
   arma::vec lambdaSeq = arma::vec();
   if (lSeq.isNotNull()) {
     lambdaSeq = Rcpp::as<arma::vec>(lSeq);
@@ -475,21 +471,24 @@ Rcpp::List cvHuberLasso(const arma::mat& X, const arma::vec& Y,
   }
   int size = n / nfolds;
   arma::vec mse = arma::zeros(nlambda);
+  int low, up;
+  arma::uvec idx, idxComp;
+  Rcpp::List hLassoList;
+  arma::vec thetaHat(d + 1);
   for (int i = 0; i < nlambda; i++) {
     for (int j = 0; j < nfolds; j++) {
-      int low = j * size;
-      int up = (j == (nfolds - 1)) ? (n - 1) : ((j + 1) * size - 1);
-      arma::uvec idx = getIndex(n, low, up);
-      arma::uvec idxComp = getIndexComp(n, low, up);
-      Rcpp::List hLassoList = huberLasso(Z.rows(idxComp), Y.rows(idxComp), lambdaSeq(i), -1, 
-                                         constTau, phi0, gamma, epsilon_c, iteMax);
-      arma::vec thetaHat = Rcpp::as<arma::vec>(hLassoList["beta"]);
+      low = j * size;
+      up = (j == (nfolds - 1)) ? (n - 1) : ((j + 1) * size - 1);
+      idx = getIndex(n, low, up);
+      idxComp = getIndexComp(n, low, up);
+      hLassoList = huberLasso(Z.rows(idxComp), Y.rows(idxComp), lambdaSeq(i), -1, constTau, phi0, 
+                              gamma, epsilon_c, iteMax);
+      thetaHat = Rcpp::as<arma::vec>(hLassoList["beta"]);
       mse(i) += pairPred(Z.rows(idx), Y.rows(idx), thetaHat);
     }
   }
   arma::uword cvIdx = mse.index_min();
-  Rcpp::List hLassoList = huberLasso(Z, Y, lambdaSeq(cvIdx), -1, constTau, phi0, gamma, 
-                                     epsilon_c, iteMax);
+  hLassoList = huberLasso(Z, Y, lambdaSeq(cvIdx), -1, constTau, phi0, gamma, epsilon_c, iteMax);
   arma::vec theta = Rcpp::as<arma::vec>(hLassoList["beta"]);
   Rcpp::List listMean = huberMean(Y - Z.cols(1, d) * theta.rows(1, d));
   theta(0) = listMean["mu"];
