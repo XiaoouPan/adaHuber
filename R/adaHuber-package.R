37bdde1aac5604c17b50790ce284032984b1c267
XiaoouPan <xip024@ucsd.edu>
2022-02-27 22:08:04 -0800

    package manual

---
+++ R/adaHuber-package.R
@@ -0,0 +1,9 @@
+#' @docType package
+#' @name adaHuber-package
+#' @title adaHuber: Adaptive Huber Estimation and Regression
+#' @description Huber-type robust estimation for mean, covariance and (penalized) regression.
+#' @author Xiaoou Pan <xip024@ucsd.edu> and Wen-Xin Zhou <wez243@ucsd.edu>
+#' @references Ke, Y., Minsker, S., Ren, Z., Sun, Q. and Zhou, W.-X. (2019). User-friendly covariance estimation for heavy-tailed distributions. Statis. Sci. 34 454-471.
+#' @references Sun, Q., Zhou, W.-X. and Fan, J. (2020). Adaptive Huber regression. J. Amer. Stat. Assoc. 115 254-265.
+#' @references Wang, L., Zheng, C., Zhou, W. and Zhou, W.-X. (2021). A new principle for tuning-free Huber regression. Stat. Sinica 31 2153-2177.
+NULL
